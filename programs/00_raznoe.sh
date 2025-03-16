913489302b6ae8123936fc966e9142eaaf157e2b
krillan <pushkinidze49@gmail.com>
2025-03-17 01:51:57 +0300

    jfhfhtgfhtthg

---
+++ programs/00_raznoe.sh
@@ -0,0 +1,16 @@
+echo '                                              Far'
+
+# https://github.com/elfmz/far2l  - установка Far на Линукс
+
+# Для Linux семейства Ubuntu установка обычно сводится к двум командам в терминале:
+# $ sudo apt-get update
+# $ sudo apt-get install mc
+
+# (после этого можно вводить “mc”, чтобы запустился Midnight Commander)
+
+
+
+echo '                                           OBS Studio'
+
+# Установка на Линукс Убунту
+# sudo apt install obs-studio

fb0752fd3a4ca1613f595cc557fa122a9036a20d
krillan <Pushkinidze49@gmail.com>
2025-05-12 21:48:26 +0300

    jgfhfhdfg

---
+++ programs/postgre.sh
@@ -0,0 +1,6 @@
+echo "                                    Библиотеки PostgreSQL"
+
+# ужно установить пакеты `libpq-dev` (для Debian/Ubuntu)
+
+# $ sudo apt-get update
+# $ sudo apt-get install libpq-dev
